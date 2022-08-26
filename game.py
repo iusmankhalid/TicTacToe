49f0b9e63f1c826c9df6ed6a3d6af66cc8684b3d
iusmankhalid <86885061+iusmankhalid@users.noreply.github.com>
2022-08-25 23:52:30 -0500

    Update game.py

--- game.py
+++ game.py
@@ -58,41 +58,6 @@ class TicTacToe:
          return False
 
 
-
-
-
-
-
-# def play(game, x_player, o_player, print_game = True):
-#     if print_game:
-#         game.print_board_nums()
-
-#     letter = 'X' 
-
-#     while game.empty_squares():
-#         if letter == 'O':
-#             square = o_player.get_move(game)
-#         else:
-#             square = x_player.get_move(game)
-        
-#         if game.make_move(square, letter):
-#             if print_game:
-#                 print(letter + ' makes a move to square {}'.format(square))
-#                 game.print_board()
-#                 print('')
-#                 if game.current_winner:
-#                     if print_game:
-#                         print(letter + ' wins!')
-#                     return letter
-
-#                 letter = 'O' if letter == 'X' else letter == 'O'
-
-#                 if print_game:
-#                     print('It\'s a tie!')
-
-
-
-
 def play(game, x_player, o_player, print_game=True):
 
     if print_game:
