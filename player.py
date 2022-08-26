b6456a29fe1f5874c1995892876294876ca0dee8
iusmankhalid <86885061+iusmankhalid@users.noreply.github.com>
2022-08-25 23:53:10 -0500

    Update player.py

--- player.py
+++ player.py
@@ -19,23 +19,6 @@ class RandomComputerPlayer(Player):
         return square
 
 
-# class HumanPlayer(Player):
-#     def __int__(self, letter):
-#         super().__int__(letter)
-
-#     def get_move(self, game):
-#         valid_square = False
-#         val = None
-#         while not valid_square:
-#              square = input(self.letter + '\'s turn. Input move(0-9)')
-#              try:
-#                  val = int(square)
-#                  if val not in game.available_moves():
-#                      raise ValueError
-#              except ValueError:
-#                 print('Invalid Square. Try again....')
-#         return val
-
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
